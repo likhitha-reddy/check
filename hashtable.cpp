bf9c2311d2b4be5225525a3c239682a64c9d5b82
likhitha <likhitha0341@gmail.com>
2023-07-07 19:28:51 +0530

    push into test2

--- hashtable.cpp
+++ hashtable.cpp
@@ -2,7 +2,20 @@
 #include <cstring>
 using namespace std;
 const int SIZE = 10;
+class Node
+{
+public:
+  int key;
+  int value;
+  Node *next;
 
+  Node(int key, int value)
+  {
+    this->key = key;
+    this->value = value;
+    this->next = nullptr;
+  }
+};
 class Node2
 {
 public:
